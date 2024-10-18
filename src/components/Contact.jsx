795f3ccae98b53e587f52cb875963341517b6e58
Aina Mitantsoa <ainamitantsogit@gmail.com>
2024-10-18 11:14:05 +0300

    footer design

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,46 +1,98 @@
 import React from "react";
-import { Github, Linkedin, Mail } from "lucide-react";
+import { Mail, Phone, Github, Linkedin } from "lucide-react";
 
 const Contact = () => {
   return (
     <section id="contact" className="my-16">
-      <h2 className="mb-4 text-3xl font-bold text-primary">
-        Entrons en contact
-      </h2>
+      <h3 className="mb-4 text-3xl font-bold text-primary">Contact</h3>
       <div className="p-6 border-l-4 rounded-lg shadow-md bg-gradient-to-r from-white to-gray-50 border-secondary">
-        <p className="mb-6 text-lg text-gray-700">
-          Vous avez un projet, une idée ou une opportunité intéressante à
-          discuter ? Je suis toujours disponible pour échanger et explorer de
-          nouvelles collaborations. N'hésitez pas à me joindre via les
-          plateformes suivantes :
-        </p>
-        <div className="flex flex-row justify-center space-x-6">
+        {/* Appeler Hanja */}
+        <p className="flex items-center mb-4">
           <a
-            href="mailto:ainamitantso@gmail.com"
-            className="flex items-center text-gray-700 transition-transform transform hover:text-secondary hover:scale-110"
+            aria-label="Appeler Hanja"
+            href="tel:+261346211520"
+            data-position="top"
+            data-tooltip="Appeler Hanja"
+            className="inline-block p-4 mr-4 bg-gray-200 rounded-full hover:bg-light btn-floating btn-large waves-effect waves-light "
           >
-            <Mail className="w-8 h-8 mr-2" />
-            <span className="text-lg font-semibold">Email</span>
+            <Phone className="w-6 h-6" />
           </a>
           <a
+            aria-label="Appeler Hanja"
+            href="tel:+261346211520"
+            className="text-lg font-semibold hover:underline"
+          >
+            +261 34 62 115 20
+          </a>
+        </p>
+
+        {/* Email Hanja */}
+        <p className="flex items-center mb-4">
+          <a
+            aria-label="Email Hanja"
+            href="mailto:ainamitantso@gmail.com"
+            data-position="top"
+            data-tooltip="Email Aina"
+            className="inline-block p-4 mr-4 bg-gray-200 rounded-full btn-floating btn-large waves-effect waves-light hover:bg-light"
+          >
+            <Mail className="w-6 h-6" />
+          </a>
+          <a
+            aria-label="Email Hanja"
+            href="mailto:ainamitantso@gmail.com"
+            className="text-lg font-semibold hover:underline"
+          >
+            ainamitantso@gmail.com
+          </a>
+        </p>
+
+        {/* GitHub Hanja */}
+        <p className="flex items-center mb-4">
+          <a
+            aria-label="Regarder Hanja sur GitHub"
             href="https://github.com/mitantsoa1"
             target="_blank"
             rel="noopener noreferrer"
-            className="flex items-center text-gray-700 transition-transform transform hover:text-secondary hover:scale-110"
+            data-position="top"
+            data-tooltip="Regarder Hanja sur GitHub"
+            className="inline-block p-4 mr-4 bg-gray-200 rounded-full btn-floating btn-large waves-effect waves-light hover:bg-light"
           >
-            <Github className="w-8 h-8 mr-2" />
-            <span className="text-lg font-semibold">GitHub</span>
+            <Github className="w-6 h-6" />
           </a>
           <a
+            aria-label="Regarder Hanja sur GitHub"
+            href="https://github.com/mitantsoa1"
+            className="text-lg font-semibold hover:underline"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            github.com/mitantsoa1
+          </a>
+        </p>
+
+        {/* LinkedIn Hanja */}
+        <p className="flex items-center mb-4">
+          <a
+            aria-label="Regarder Hanja sur LinkedIn"
             href="https://www.linkedin.com/in/mitantsoa-rakotomalala-60a122186/"
             target="_blank"
             rel="noopener noreferrer"
-            className="flex items-center text-gray-700 transition-transform transform hover:text-secondary hover:scale-110"
+            data-position="top"
+            data-tooltip="Regarder Hanja sur LinkedIn"
+            className="inline-block p-4 mr-4 bg-gray-200 rounded-full btn-floating btn-large waves-effect waves-light hover:bg-light"
           >
-            <Linkedin className="w-8 h-8 mr-2" />
-            <span className="text-lg font-semibold">LinkedIn</span>
+            <Linkedin className="w-6 h-6" />
           </a>
-        </div>
+          <a
+            aria-label="Regarder Hanja sur LinkedIn"
+            href="https://www.linkedin.com/in/mitantsoa-rakotomalala-60a122186/"
+            className="text-lg font-semibold hover:underline"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            linkedin.com/in/mitantsoa-rakotomalala-60a122186
+          </a>
+        </p>
       </div>
     </section>
   );
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,26 @@
 import React from "react";
 
-function Footer() {
-  return <footer className="relative bg-gris"></footer>;
-}
+const Footer = () => {
+  return (
+    <footer className="relative py-0 text-center bg-gray-200">
+      <div className="footer-content">
+        <p className="pt-4 text-sm"> All rights reserved © H. Mitantsoa 2024</p>
+        <p className="mt-2 text-gray-700">&nbsp;</p>
+      </div>
+      <div className="absolute bottom-0 left-0 w-full overflow-hidden leading-none footer-wave">
+        <svg
+          viewBox="0 0 1200 100"
+          preserveAspectRatio="none"
+          className="w-full h-20"
+        >
+          <path
+            d="M0,0 C600,100 600,100 1200,0 L1200,100 L0,100 Z"
+            className="fill-dark"
+          ></path>
+        </svg>
+      </div>
+    </footer>
+  );
+};
 
 export default Footer;
